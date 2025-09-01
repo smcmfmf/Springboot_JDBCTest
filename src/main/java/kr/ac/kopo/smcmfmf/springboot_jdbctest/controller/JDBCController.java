a7a081231569bed95659d3f6c16c264b18ef921b
smcmfmf <hk0831bae@gmail.com>
2025-09-01 11:27:19 +0900

    데이터 삭제 기능 추가

--- src/main/java/kr/ac/kopo/smcmfmf/springboot_jdbctest/controller/JDBCController.java
+++ src/main/java/kr/ac/kopo/smcmfmf/springboot_jdbctest/controller/JDBCController.java
@@ -6,10 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,6 +37,29 @@ public class JDBCController {
 //        jdbcTemplate.update(sql, person.getName(), person.getAge(), person.getEmail());
         Object[] params = {person.getName(), person.getAge(), person.getEmail()}; // 오브젝트 배열로 저장 후 사용
         int resultCount = jdbcTemplate.update(sql, params); // 업데이트 한 개수를 반환함
+        return "redirect:/exam01";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editMethod(@PathVariable int id, Model model) {
+        String sql = "select * from person where id = ?"; // id를 가지고 모든 데이터를 조회하도록 함
+        Person person = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Person.class), id);
+        model.addAttribute("person", person);
+        return "viewPage01_edit";
+    }
+
+    @PostMapping("/update")
+    public String updateMethod(@ModelAttribute("Person") Person person) {
+        String sql = "update person set name = ?, age = ?, email = ? where id = ?";
+        Object[] params = {person.getName(), person.getAge(), person.getEmail(), person.getId()};
+        int resultCount = jdbcTemplate.update(sql, params);
+        return "redirect:/exam01"; // 수정된 내용을 출력
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMethod(@PathVariable(name = "id") int id) {
+        String sql = "delete from person where id = ?";
+        int resultCount = jdbcTemplate.update(sql, id);
         return "redirect:/exam01";
     }
 }
